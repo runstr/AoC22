7ef27377187437a115cfa127b88412d47d067e05
runstr <runestro18@gmail.com>
2022-11-30 23:43:05 +0100

    ready!

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from day_imports import *
 if __name__ == '__main__':
     day1_1.execution()
     todays_date = get_todays_date()
-    todays_date = 1
-    # insert_data(todays_date)
+    insert_data(todays_date)
     exec("day{}_1.execution()".format(todays_date))
     exec("day{}_2.execution()".format(todays_date))
