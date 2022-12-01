443bfb2475e01b0c41887f17d313593c3c5fbf21
runstr <runestro18@gmail.com>
2022-12-01 07:07:28 +0100

    day1

--- Day1/day1_1.py
+++ Day1/day1_1.py
@@ -5,7 +5,21 @@ from aocd import submit
 filepath = pathlib.Path(__file__).parent.resolve()
 
 
+def get_most_calories():
+    data = load_data_as_lines(filepath, example=False)
+    max_calories = 0
+    total_calories = 0
+    for line in data:
+        if line == "":
+            if total_calories>max_calories:
+                max_calories=total_calories
+            total_calories = 0
+        else:
+            total_calories += int(line)
+    return max_calories
+
+
 @timeexecution
 def execution():
-    print(load_data(filepath, example=True))
-    # submit(my_answer, part="a", day=1, year=2022)
+    print(get_most_calories())
+    #submit(get_most_calories(), part="a", day=1, year=2022)
--- Day1/day1_2.py
+++ Day1/day1_2.py
@@ -5,7 +5,39 @@ from aocd import submit
 filepath = pathlib.Path(__file__).parent.resolve()
 
 
+def get_most_calories():
+    data = load_data_as_lines(filepath, example=False)
+    max_calories_1 = 0
+    max_calories_2 = 0
+    max_calories_3 = 0
+    total_calories = 0
+    for line in data:
+        if line == "":
+            if total_calories >= max_calories_1:
+                max_calories_3 = max_calories_2
+                max_calories_2 = max_calories_1
+                max_calories_1 = total_calories
+            elif total_calories >= max_calories_2:
+                max_calories_3 = max_calories_2
+                max_calories_2 = total_calories
+            elif total_calories >= max_calories_3:
+                max_calories_3 = total_calories
+            total_calories = 0
+        else:
+            total_calories += int(line)
+    if total_calories >= max_calories_1:
+        max_calories_3 = max_calories_2
+        max_calories_2 = max_calories_1
+        max_calories_1 = total_calories
+    elif total_calories >= max_calories_2:
+        max_calories_3 = max_calories_2
+        max_calories_2 = total_calories
+    elif total_calories >= max_calories_3:
+        max_calories_3 = total_calories
+    return max_calories_1+max_calories_2+max_calories_3
+
+
 @timeexecution
 def execution():
-    print(load_data(filepath, example=False))
-    # submit(my_answer, part="a", day=1, year=2022)
+    answer = get_most_calories()
+    #submit(answer, part="b", day=1, year=2022)
