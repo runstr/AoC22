b9f35771e31ec7b4ab87beaabe74efc4f895b736
Rune Strømholt <rune.stromholt@silabs.com>
2023-12-07 11:12:50 +0100

    Analytic solution to day 6

--- y2023/Day6/day6_1.py
+++ y2023/Day6/day6_1.py
@@ -2,8 +2,16 @@ import pathlib
 from Tools.tools import load_data_as_lines, load_data, load_data_as_int, timeexecution
 from aocd import submit
 import numpy
-
+import math
 filepath = pathlib.Path(__file__).parent.resolve()
+
+def get_possible_ways_to_win_analytic(time, record):
+    """ solve equation x**2 -time*x + record = 0. To get the record we x to be higher than x1
+    less than x2"""
+    x1 = math.floor((time - math.sqrt(time**2 - 4*record))/2 +1)
+    x2 = math.ceil((time + math.sqrt(time ** 2 - 4 * record)) / 2 -1)
+    return len(range(x1, x2+1))
+
 
 def get_possible_ways_to_win(time, record):
     total_distance = []
@@ -20,7 +28,7 @@ def get_possible_ways_to_win(time, record):
 
 
 def get_my_answer():
-    time, distance = load_data_as_lines(filepath, example=False)
+    time, distance = load_data_as_lines(filepath, example=True)
     time = time.split(" ")
     distance = distance.split(" ")
     time = list(map(int, [i for i in time if i != ""][1:]))
@@ -29,9 +37,8 @@ def get_my_answer():
     for i in range(len(time)):
         race_time = time[i]
         record_distance = distance[i]
-        possible_ways_to_win.append(get_possible_ways_to_win(race_time, record_distance))
-    my_answer = numpy.prod(possible_ways_to_win)
-    return my_answer
+        possible_ways_to_win.append(get_possible_ways_to_win_analytic(race_time, record_distance))
+    return possible_ways_to_win
 
 
 @timeexecution
--- y2023/Day6/day6_2.py
+++ y2023/Day6/day6_2.py
@@ -1,8 +1,16 @@
+import math
 import pathlib
 from Tools.tools import load_data_as_lines, load_data, load_data_as_int, timeexecution
 from aocd import submit
 filepath = pathlib.Path(__file__).parent.resolve()
 import numpy
+
+def get_possible_ways_to_win_analytic(time, record):
+    """ solve equation x**2 -time*x + record = 0. To get the record we x to be higher than x1
+    less than x2"""
+    x1 = math.floor((time - math.sqrt(time**2 - 4*record))/2 +1)
+    x2 = math.ceil((time + math.sqrt(time ** 2 - 4 * record)) / 2 -1)
+    return len(range(x1, x2+1))
 
 def get_possible_ways_to_win(time, record):
     total_distance = []
@@ -13,8 +21,8 @@ def get_possible_ways_to_win(time, record):
         total_distance.append(distance)
     wins = 0
     for i in total_distance:
-        if i>record:
-            wins+=1
+        if i > record:
+            wins += 1
     return wins
 
 
@@ -22,7 +30,7 @@ def get_my_answer():
     time, distance = load_data_as_lines(filepath, example=False)
     time = int(time.replace(" ", "").split(":")[1])
     distance = int(distance.replace(" ", "").split(":")[1])
-    possible_ways_to_win = (get_possible_ways_to_win(time, distance))
+    possible_ways_to_win = (get_possible_ways_to_win_analytic(time, distance))
     return possible_ways_to_win
 
 
