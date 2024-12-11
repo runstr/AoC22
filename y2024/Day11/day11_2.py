bf0f8606aad6042504bb3c8824f41666edea53ed
Rune Strømholt <rune.stromholt@silabs.com>
2024-12-11 10:28:39 +0100

    Some finetuning on day 11

--- y2024/Day11/day11_2.py
+++ y2024/Day11/day11_2.py
@@ -10,21 +10,24 @@ def rules(number):
 If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
 If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone
     """
-    if number == "0":
-        return ["1"]
-    if len(number) % 2 == 0:
-        return [str(int(number[:len(number) // 2])), str(int(number[len(number) // 2:]))]
-    return [str(int(number) * 2024)]
+    if number == 0:
+        return [1]
+    length = len(str(number))
+    if length % 2 == 0:
+        return [int(str(number)[:length // 2]), int(str(number)[length // 2:])]
+    return [int(number) * 2024]
 
 
 def get_my_answer():
-    data = load_data(filepath, example=False).split(" ")
+    data = map(int, load_data(filepath, example=False).split(" "))
     i = 0
     numbers = {}
     for j in data:
         numbers[j] = 1
+    new_length = 0
     while i < 75:
         new_numbers = {}
+        new_length = 0
         for num, value in numbers.items():
             new_nums = rules(num)
             for new_num in new_nums:
@@ -32,10 +35,10 @@ def get_my_answer():
                     new_numbers[new_num] += value
                 else:
                     new_numbers[new_num] = value
+                new_length+=value
         numbers = new_numbers
         i += 1
-    return sum(numbers.values())
-
+    return new_length
 
 @timeexecution
 def execution():
