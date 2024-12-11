34c9c0a0fd2518add18c29d91b1376bd7942c179
Rune Strømholt <rune.stromholt@silabs.com>
2024-12-11 09:58:00 +0100

    Day 11

--- y2024/Day11/day11_1.py
+++ y2024/Day11/day11_1.py
@@ -4,10 +4,30 @@ from aocd import submit
 
 filepath = pathlib.Path(__file__).parent.resolve()
 
+def rules(number):
+    """
+    If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone
+    """
+    if number == "0":
+        return ["1"]
+    if len(number) % 2 == 0:
+        return [str(int(number[:len(number)//2])), str(int(number[len(number)//2:]))]
+    return [str(int(number)*2024)]
+
 
 def get_my_answer():
-    my_answer = load_data(filepath, example=True)
-    return my_answer
+    data = load_data(filepath, example=False).split(" ")
+    i=0
+    new_list = data
+    while i < 25:
+        old_list = new_list
+        new_list = []
+        for j in old_list:
+            new_list += rules(j)
+        i+=1
+    return len(new_list)
 
 
 @timeexecution
--- y2024/Day11/day11_2.py
+++ y2024/Day11/day11_2.py
@@ -4,9 +4,37 @@ from aocd import submit
 filepath = pathlib.Path(__file__).parent.resolve()
 
 
+def rules(number):
+    """
+    If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone
+    """
+    if number == "0":
+        return ["1"]
+    if len(number) % 2 == 0:
+        return [str(int(number[:len(number) // 2])), str(int(number[len(number) // 2:]))]
+    return [str(int(number) * 2024)]
+
+
 def get_my_answer():
-    my_answer = load_data(filepath, example=True)
-    return my_answer
+    data = load_data(filepath, example=False).split(" ")
+    i = 0
+    numbers = {}
+    for j in data:
+        numbers[j] = 1
+    while i < 75:
+        new_numbers = {}
+        for num, value in numbers.items():
+            new_nums = rules(num)
+            for new_num in new_nums:
+                if new_num in new_numbers:
+                    new_numbers[new_num] += value
+                else:
+                    new_numbers[new_num] = value
+        numbers = new_numbers
+        i += 1
+    return sum(numbers.values())
 
 
 @timeexecution
