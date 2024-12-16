16160a7a43ca5a60b80bd5cade760847c8216160
Rune Strømholt <rune.stromholt@silabs.com>
2024-12-16 09:41:41 +0100

    Some improvements and making sure part 2 is independent of part 1

--- y2024/Day16/day16_1.py
+++ y2024/Day16/day16_1.py
@@ -5,23 +5,29 @@ from aocd import submit
 
 filepath = pathlib.Path(__file__).parent.resolve()
 
-
 def get_my_answer():
     data = load_data_as_lines(filepath, example=False)
-    cave_map ={}
+    cave_map = {}
     start_point = ()
+    end_point = ()
     for y in range(len(data)):
         for x in range(len(data[0])):
-            cave_map[(x,y)] = data[y][x]
+            cave_map[(x, y)] = data[y][x]
             if data[y][x] == "S":
                 start_point = (x, y)
+            if data[y][x] == "E":
+                end_point = (x, y)
+    return run_bfs(cave_map, start_point, end_point)
+
+
+def run_bfs(cave_map, start_point, end_point):
     visited = dict()
     next_paths = [(start_point, (1, 0), 0, 0)]
     minimum_score = math.inf
     while next_paths:
         path, direction, turns, moves = next_paths.pop(0)
         total_score = turns*1000 + moves
-        if total_score > minimum_score:
+        if total_score > minimum_score or total_score + check_difference(path, end_point) > minimum_score:
             continue
         if cave_map[path] == "E":
             if total_score < minimum_score:
@@ -39,10 +45,17 @@ def get_my_answer():
                 next_paths.append((new_point, (dx, dy), turns, moves + 1))
             else:
                 next_paths.append((new_point, (dx, dy), turns+1, moves + 1))
-
     return minimum_score
 
 
+def check_difference(this_point, other_point):
+    dx = this_point[0] - other_point[0]
+    dy = this_point[1] - other_point[1]
+    minimum_difference = abs(dx) + abs(dy)
+    if dx == 0 or dy == 0:
+        return minimum_difference
+    else:
+        return minimum_difference +1000
 @timeexecution
 def execution():
     submit_answer = False
--- y2024/Day16/day16_2.py
+++ y2024/Day16/day16_2.py
@@ -3,26 +3,31 @@ import math
 import pathlib
 from Tools.tools import load_data_as_lines, load_data, load_data_as_int, timeexecution
 from aocd import submit
+from .day16_1 import run_bfs
 filepath = pathlib.Path(__file__).parent.resolve()
 
 
 def get_my_answer():
+
     data = load_data_as_lines(filepath, example=False)
     cave_map = {}
     start_point = ()
+    end_point = ()
     for y in range(len(data)):
         for x in range(len(data[0])):
             cave_map[(x, y)] = data[y][x]
             if data[y][x] == "S":
                 start_point = (x, y)
+            if data[y][x] == "E":
+                end_point = (x, y)
+    minimum_score = run_bfs(cave_map, start_point, end_point)
     visited = dict()
     next_paths = [(start_point, (1, 0), 0, 0, str(start_point)+":")]
-    minimum_score = 105496
     all_paths = []
     while next_paths:
         path, direction, turns, moves, full_path = next_paths.pop(0)
         total_score = turns*1000 + moves
-        if total_score > minimum_score:
+        if total_score > minimum_score or total_score + check_difference(path, end_point) > minimum_score:
             continue
         if cave_map[path] == "E":
             if total_score == minimum_score:
@@ -35,29 +40,35 @@ def get_my_answer():
         for dx, dy in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
             if (dx, dy) == (-direction[0], -direction[1]):
                 continue
-            new_point =(path[0]+dx, path[1]+dy)
+            new_point = (path[0]+dx, path[1]+dy)
             if cave_map[new_point] == "#":
                 continue
             if (dx, dy) == direction:
-                new_turns = turns
-            else:
-                new_turns = turns+1
-            new_moves = moves+1
-            next_paths.append((new_point, (dx, dy), new_turns, new_moves, full_path+str(new_point)+":"))
+                next_paths.append((new_point, (dx, dy), turns, moves+1, full_path + str(new_point) + ":"))
 
-    paths =set()
+            else:
+                next_paths.append((new_point, (dx, dy), turns+1, moves+1, full_path + str(new_point) + ":"))
+
+    paths = set()
     for path in all_paths:
         all_points = path.split(":")
         for point in all_points:
             try:
-                x,y = tuple(map(int,(point[1:-1].split(", "))))
-                paths.add((x,y))
+                x, y = tuple(map(int,(point[1:-1].split(", "))))
+                paths.add((x, y))
             except:
                 pass
 
     return len(paths)
 
-
+def check_difference(this_point, other_point):
+    dx = this_point[0] - other_point[0]
+    dy = this_point[1] - other_point[1]
+    minimum_difference = abs(dx) + abs(dy)
+    if dx == 0 or dy == 0:
+        return minimum_difference
+    else:
+        return minimum_difference +1000
 @timeexecution
 def execution():
     submit_answer = False
